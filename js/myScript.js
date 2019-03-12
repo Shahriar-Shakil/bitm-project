e26b6fe14a1dddb86651cee0b0a63c05eeca93b1
Shahriar-Shakil <toshahriarshakil@gmail.com>
2019-03-13 00:20:38 +0600

    bitm-project

---
+++ js/myScript.js
@@ -0,0 +1,29 @@
+$('.portfolio-item').isotope({
+    itemSelector: '.item',
+    mode        : 'fitRows'
+});
+
+$('.portfolio-menu ul li').click(function (){
+
+    $('.portfolio-menu ul li').removeClass('active');
+    $(this).addClass('active');
+
+    var selector = $(this).attr('data-filter');
+    $('.portfolio-item').isotope({
+        filter: selector
+    });
+});
+
+
+/*test*/
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
